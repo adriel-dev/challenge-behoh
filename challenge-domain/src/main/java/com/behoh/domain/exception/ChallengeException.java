57e3ffc1a9badbf146986e09b3cda80cb07e4b9d
Adriel Felix <adrielandrade3@gmail.com>
2024-08-10 20:05:09 -0300

    custom exceptions

---
+++ challenge-domain/src/main/java/com/behoh/domain/exception/ChallengeException.java
@@ -0,0 +1,8 @@
+package com.behoh.domain.exception;
+
+public class ChallengeException extends RuntimeException {
+    public ChallengeException() {}
+    public ChallengeException(String message) {
+        super(message);
+    }
+}
